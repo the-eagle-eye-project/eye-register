0943088b0b399f525785942d89a08bc61ff41c1a
jmart <jmartinez5120@gmail.com>
2023-06-26 23:23:58 -0400

    Added the starting point of the source code for the eye-register module.

---
+++ src/main/java/com/theeagleeyeproject/eyeregister/EyeRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.theeagleeyeproject.eyeregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theeagleeyeproject/eyeregister/EyeRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theeagleeyeproject.eyeregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
